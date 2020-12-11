7fb2ca8d5761018511ae737885508a004c4509df
sunitha1238 <75845236+sunitha1238@users.noreply.github.com>
2020-12-12 02:25:21 +0530

    Create output_all_approaches.java

---
+++ output_all_approaches.java
@@ -0,0 +1,22 @@
+Reading file using FileInputStream and Scanner
+Time taken for Reading the File and count the word occurance:  76221
+Total Occurances of the Given Search String:  1055
+
+
+
+
+Reading file using BufferReader
+Time taken for Reading the File and count the word occurance  :  18370
+Total Occurances of the Given Search String:  1055
+
+
+
+Reading file using org.apache.commons.io.LineIterator
+Time taken for Reading the File and count the word occurance:  21294
+Total Occurances of the Given Search String:  1055
+
+
+
+Reading file using SeekableByteChannel
+Time taken for Reading the File and count the word occurance:  20381
+Total Occurances of the Given Search String:  948
