ed28d161a4f0cfc4f0c8d459e4330c948dff8962
sunitha1238 <75845236+sunitha1238@users.noreply.github.com>
2020-12-12 02:10:12 +0530

    ReadLargeUnicodeFileUsingFileInputStreamandScanner

---
+++ ReadUnicodeFileUsingFileInputStream.java
@@ -0,0 +1,59 @@
+package practice;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ReadUnicodeFileUsingFileInputStream {
+
+	public static void main(String[] args) throws IOException {
+		try {
+			File fileDir = new File("c:\\itcont.txt");
+			  //provide the Search String here..
+			String searchString = "LINCOLN";
+
+			FileInputStream inputStream = new FileInputStream(fileDir);
+			Scanner sc = new Scanner(inputStream, "UTF-8");
+
+			Instant wordcountStart = Instant.now();
+			int wordcount = 0;
+			System.out.println("Reading file using FileInputStream and Scanner");
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] wordsPerLine = line.split(" ");
+				
+				/*for (String word : wordsPerLine) {
+	                 if (word.equals(searchString))   //Search for the given word
+	                 {
+	                   wordcount++;    //If Present increase the count by one
+	                 }
+		        }*/
+				//observed little less time taken when used ArrayList for searching a word
+				ArrayList<String> wordsPerLineList = new ArrayList<String>(
+						Arrays.asList(wordsPerLine));
+				
+				if (wordsPerLineList.contains(searchString))
+					wordcount++;
+				
+			}
+			Instant wordcountend = Instant.now();
+			long timeTaken = Duration.between(wordcountStart, wordcountend)
+					.toMillis();
+			System.out.println("Time taken for Reading the File and count the word occurance:  "
+					+ timeTaken);
+			System.out.println("Total Occurances of the Given Search String:  " + wordcount);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+}
